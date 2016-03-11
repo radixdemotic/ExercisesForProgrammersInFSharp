7fdffb7b1689be296a6a0e3e276a1635d49aeeed
radixdemotic <radixdemotic@gmail.com>
2016-03-10 19:06:27 -0800

    Doing Exercises For Programmers in FSharp
    First Commit

---
+++ Hello/Hello.fs
@@ -0,0 +1,7 @@
+﻿module ExercisesForProgrammers.Hello
+
+open System
+
+printf "What is your name? "
+printf "Hello, %s, nice to meet you!" (System.Console.ReadLine())
+System.Console.ReadKey() |> ignore
